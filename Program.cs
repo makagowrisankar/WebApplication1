8d05ce3cb9a73cd01470a41a876dc7e563697abb
gowri sankar <makagowrishankarshankar@gmail.com>
2026-04-19 15:33:07 +0530

    "fix: sqlite correct package"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ✅ DB connection
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data Source=app.db"));
 
 var app = builder.Build();
 
